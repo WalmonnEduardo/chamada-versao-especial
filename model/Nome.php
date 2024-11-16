2fe2b38fe58ce5e7663ef06952feadf1fe504af9
Walmonn Eduardo <130590544+WalmonnEduardo@users.noreply.github.com>
2024-11-16 20:52:38 -0300

    Create Nome.php

---
+++ model/Nome.php
@@ -0,0 +1,24 @@
+<?php
+class Nome
+{
+    protected string $nome;
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+}
+?>
