9e6936a9b1132093db45e0077c26c064c17c49b9
Walmonn Eduardo <130590544+WalmonnEduardo@users.noreply.github.com>
2024-11-21 13:08:01 -0300

    Update Aluno.php

--- model/Aluno.php
+++ model/Aluno.php
@@ -1,11 +1,28 @@
 <?php
 require_once("Nome.php");
-class Aluno extends Nome
+class Aluno extends Nome implements JsonSerializable
 {
     private int $numMatricula;
     private array $presenca;
-
-    
+    public static function criarAlunos(array $recolherdados)
+    {
+        for($i = 0 ; $i < count($recolherdados) ; $i++)
+        {
+            $alunos[$i] = new Aluno();
+            $alunos[$i]->setNome($recolherdados[$i]['nome']);
+            $alunos[$i]->setNumMatricula($recolherdados[$i]['numMatricula']);
+            $alunos[$i]->setPresenca($recolherdados[$i]['presenca']);
+        }
+        return $alunos;
+    }
+    public function jsonSerialize(): mixed
+    {
+        return $aluno = [
+            "nome" => $this->getNome(),
+            "numMatricula" => $this->getNumMatricula(),
+            "presenca" => $this->getPresenca()
+        ];
+    }
     public function __construct()
     {
         $this->presenca = array();
